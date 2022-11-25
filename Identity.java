f60f515925d9c7d2a7d2e0b93a9a2f0330670712
SHREYASI DAS <104278102+Shreyasi-21@users.noreply.github.com>
2022-11-25 11:02:22 +0530

    Create Identity.java

---
+++ Identity.java
@@ -0,0 +1,28 @@
+public class Identity {
+
+	public static void main(String[] args) {
+		
+		int i, j, Flag = 1;
+		
+		
+		int[][] arr = {{1, 0, 0}, {0, 1, 0}, {0, 0, 1}};
+		
+		
+		for(i = 0; i < 3 ; i++)
+		{
+			for(j = 0; j < 3; j++)
+			{
+				if(arr[i][j] != 1 && arr[j][i] != 0) {
+					Flag = 0;
+					break;
+				}
+			}
+		}
+		if(Flag == 1) {
+			System.out.println("\nMatrix is an Identity Matrix");
+		}
+		else {
+			System.out.println("\nMatrix is Not an Identity Matrix");
+		}
+	}
+}
