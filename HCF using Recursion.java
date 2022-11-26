f3273b078c992b930e9ad46dafa092f0646bd16e
SHREYASI DAS <104278102+Shreyasi-21@users.noreply.github.com>
2022-11-26 12:25:18 +0530

    Create HCF using Recursion.java

---
+++ HCF using Recursion.java
@@ -0,0 +1,28 @@
+public class HCF
+{
+   
+    static int gcd(int num1, int num2)
+    {
+        
+        if (num1 == 0)
+          return num2;
+          
+        if (num2 == 0)
+          return num1;
+
+        
+        if (num1 == num2)
+            return num1;
+            
+       
+        if (num1 > num2)
+            return gcd(num1-num2, num2);
+        return gcd(num1, num2-num1);
+    }
+  
+    public static void main(String[] args)
+    {
+        int num1 = 98, num2 = 56;
+        System.out.println("GCD of " + num1 +" and " + num2 + " is " + gcd(num1, num2));
+    }
+}
